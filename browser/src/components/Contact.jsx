68e787d42b21f9226edd014858728e9fbaabb02c
Rithika-22 <ranjirithika@gmail.com>
2024-01-09 10:19:25 +0530

    Initial commit

---
+++ browser/src/components/Contact.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import Navbar from './Navbar';
+import './Contact.css';
+
+const Cart = () => {
+  return (
+    <div className="cart-container">
+      <Navbar />
+      <h3>Contact Details</h3>
+      <p>Phone number: +91 9789770197 </p>
+      <p>E-mail: ranjirithika@gmail.com</p>
+      <p>Address: S-32 Abdul kalam street Soodamanipuram Karaikudi -630003</p>
+      <p>LinkedIn:https://www.linkedin.com/in/rithika-k-a488582a1</p>
+    </div>
+  );
+};
+
+export default Cart;
